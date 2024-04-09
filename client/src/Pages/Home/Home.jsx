8a1bb95fd0242ec62a13001bb1d4b1fca592558f
Naim <skylike2904@gmail.com>
2024-04-09 12:48:21 +0600

    Travel Menus added

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { Plane, Hotel, ScrollText, Luggage, TentTree } from "lucide-react";
+
 import "./Home.scss";
 
 import { UseStateContext } from "../../Context/StateContext";
@@ -6,11 +10,41 @@ import { UseAuth } from "../../Context/AuthContext";
 import homeBgVideo from "../../assets/home-bg.mp4";
 
 const Home = () => {
+  // states:
+  const [selectedTravelMenu, setSelectedTravelMenu] = useState("Flight");
+  const [tourType, setTourType] = useState("One Way");
+
   const { test } = UseStateContext();
   const { user } = UseAuth();
 
   console.log(test);
   console.log(user);
+
+  // inner components:
+  const FlightMenuItem = ({ context, icon }) => (
+    <li
+      className={
+        selectedTravelMenu === context ? "menu-item active" : "menu-item"
+      }
+      onClick={() => setSelectedTravelMenu(context)}
+    >
+      {icon}
+      {context}
+    </li>
+  );
+
+  const TourTypeItem = ({ context }) => (
+    <div className="tour-type-item">
+      <input
+        type="radio"
+        name="TripType"
+        id={context.split(" ")[0]}
+        onChange={() => setTourType(context)}
+        checked={tourType === context}
+      />
+      <label htmlFor={context.split(" ")[0]}>{context}</label>
+    </div>
+  );
 
   return (
     <section className="home-page-container">
@@ -28,8 +62,24 @@ const Home = () => {
 
         <h2 className="subtitle">Find Flights, Hotels, Visa & Holidays</h2>
 
-        <div className="exclusive-offers-container">
-          <h3 className="title">Exclusive Offers</h3>
+        <div className="travel-form-container">
+          <ul className="menu-container">
+            <FlightMenuItem context="Flight" icon={<Plane />} />
+            <FlightMenuItem context="Hotel" icon={<Hotel />} />
+            <FlightMenuItem context="Visa" icon={<ScrollText />} />
+            <FlightMenuItem context="Holiday Package" icon={<Luggage />} />
+            <FlightMenuItem context="Umrah Package" icon={<TentTree />} />
+          </ul>
+
+          {selectedTravelMenu !== "Flight" ? (
+            <></>
+          ) : (
+            <div className="tour-type-container">
+              <TourTypeItem context="One Way" />
+              <TourTypeItem context="Round Trip" />
+              <TourTypeItem context="Multi City" />
+            </div>
+          )}
         </div>
       </div>
     </section>
